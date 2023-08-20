9ab77954da1788c538311b22b079e1f720e741ed
MaaMachado <maamachadox@gmail.com>
2023-08-20 17:12:15 -0300

    General css and start of actual styling

--- Controller/script.js
+++ Controller/script.js
@@ -2,3 +2,19 @@
 function linkInsta() {
   window.open('https://www.instagram.com/cozinhadadelia/', '_blank');
 }
+
+// Menu
+function openMenu(evt, menuName) {
+  var i, x, tablinks;
+  x = document.getElementsByClassName("menu");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablink");
+  for (i = 0; i < x.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" dark-grey", "");
+  }
+  document.getElementById(menuName).style.display = "block";
+  evt.currentTarget.firstElementChild.className += " dark-grey";
+}
+document.getElementById("myLink").click();
